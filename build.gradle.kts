4e7e255cb4adb725a503ab51276a02bfd5e35b2d
gunkim <gunkim.dev@gmail.com>
2023-01-15 00:30:38 +0900

    :tada: 프로젝트 생성

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.21"
+}
+
+group = "io.github.dev-babies"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.assertj:assertj-core:3.24.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
