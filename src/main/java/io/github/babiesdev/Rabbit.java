cadac688d2b1b0a37efd906ce2aa090a30ea4172
usa_develop <tjsrsdqq4@gmail.com>
2023-01-15 01:47:38 +0900

    feat : Rabbit 구현

---
+++ src/main/java/io/github/babiesdev/Rabbit.java
@@ -0,0 +1,19 @@
+package io.github.babiesdev;
+
+public class Rabbit {
+    public Rabbit(Position position) {
+
+    }
+
+    public void leftMove(int moveCount) {
+
+    }
+
+    public void rightMove(int moveCount) {
+
+    }
+
+    public Position position() {
+        return new Position(0);
+    }
+}
