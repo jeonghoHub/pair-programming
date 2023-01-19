b2cdbbef60b6972bdb8902955f0a73f7c6c5f5c7
usa_develop <tjsrsdqq4@gmail.com>
2023-01-19 22:20:12 +0900

    feat : Rabbit 테스트코드 구현

--- src/main/java/io/github/babiesdev/Rabbit.java
+++ src/main/java/io/github/babiesdev/Rabbit.java
@@ -2,15 +2,27 @@ package io.github.babiesdev;
 
 public record Rabbit(Position position) {
 
-    public void leftMove(int moveCount) {
-
+    public Rabbit leftMove(int moveCount) {
+        validateRange(moveCount);
+        return new Rabbit(new Position(positionValue() - moveCount));
     }
 
-    public void rightMove(int moveCount) {
-
+    public Rabbit rightMove(int moveCount) {
+        validateRange(moveCount);
+        return new Rabbit(new Position(positionValue() + moveCount));
     }
 
     public Position position() {
         return position;
+    }
+
+    public int positionValue() {
+        return position.value();
+    }
+
+    private void validateRange(int value) {
+        if(value < 1 || value > 5) {
+            throw new IllegalArgumentException(String.format("이동할 수 있는 거리는 1 ~ 5 사이의 값이어야 합니다. (현재: %d)", value));
+        }
     }
 }
