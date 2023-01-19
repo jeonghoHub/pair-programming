2b6d2398813747c87a033cc6edae94d5a3879b52
gunny <45007556+gunkim@users.noreply.github.com>
2023-01-19 21:20:05 +0900

    :white_check_mark: 잘못된 테스트 케이스 단언문 수정 (#4)

--- src/test/java/io/github/babiesdev/RabbitTests.java
+++ src/test/java/io/github/babiesdev/RabbitTests.java
@@ -1,10 +1,11 @@
 package io.github.babiesdev;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class RabbitTests {
     @ParameterizedTest
@@ -12,6 +13,8 @@ class RabbitTests {
     void 왼쪽으로_최대_5만큼_이동한다(int moveCount) {
         var rabbit = new Rabbit(new Position(0));
         rabbit.leftMove(moveCount);
+
+        assertThat(rabbit.positionValue()).isEqualTo(moveCount);
     }
 
     @ParameterizedTest
@@ -19,20 +22,28 @@ class RabbitTests {
     void 오른쪽으로_최대_5만큼_이동한다(int moveCount) {
         var rabbit = new Rabbit(new Position(0));
         rabbit.rightMove(moveCount);
+
+        assertThat(rabbit.positionValue()).isEqualTo(-moveCount);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0, 6})
     void 왼쪽으로_1미만_5초과_이동할_경우_예외가_발생한다(int moveCount) {
         var rabbit = new Rabbit(new Position(0));
-        rabbit.leftMove(moveCount);
+
+        assertThatThrownBy(() -> rabbit.leftMove(moveCount))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(String.format("이동할 수 있는 거리는 1 ~ 5 사이의 값이어야 합니다. (현재: %d)", moveCount));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0, 6})
     void 오른쪽으로_1미만_5초과_이동할_경우_예외가_발생한다(int moveCount) {
         var rabbit = new Rabbit(new Position(0));
-        rabbit.rightMove(moveCount);
+
+        assertThatThrownBy(() -> rabbit.rightMove(moveCount))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(String.format("이동할 수 있는 거리는 1 ~ 5 사이의 값이어야 합니다. (현재: %d)", moveCount));
     }
 
     @Test
